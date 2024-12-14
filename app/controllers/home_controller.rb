6ad3139de7ead66e3075c0d476ba7f810bfcadf4
Rebeka Oliveira <bekabwatriz@gmail.com>
2024-12-14 10:35:55 -0300

    Primeiro commit do projeto Base

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
   end
+
+  def index
+    @remedios = Remedio.page(params[:page]).per(5)# Carrega todos os remédios
+  end
+
 end
