26e30cf7b410530cfaf0290f5eb5ab440965a453
Rebeka Oliveira <bekabwatriz@gmail.com>
2024-12-13 23:33:18 -0300

    atz1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def after_sign_up_path_for(resource)
+    home_index_path # Redireciona para a página de login
+  end
+
+  def after_sign_in_path_for(resource)
+    home_path
+  end
+
+  # Redirecionar após logout
+  def after_sign_out_path_for(resource_or_scope)
+    unauthenticated_root_path
+  end
+
+  #before_action :authenticate_user!
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #get "home/index"
+  devise_for :user
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+
+
+  #get "up" => "rails/health#show", as: :rails_health_check
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+    
+  end
+
+  authenticated :user do
+    root to: 'home#index', as: :authenticated_root
+  end
+
+  # Página de login
+  unauthenticated do
+    root to: 'devise/sessions#new', as: :unauthenticated_root
+  end
+
+  # Outras rotas
+  get 'home', to: 'home#index'
+
+  #get "/test_home", to: "home#index" # Rota para testar o HTML diretamente
+
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
